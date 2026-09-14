df04e3e8462dc7d8294aa3d99cedbb93d862da56
laibishak200743-sudo <laibishak200743@gmail.com>
2026-09-14 10:58:09 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.whattocook.app',
+  appName: 'What To Cook',
+  webDir: 'dist'
+};
+
+export default config;
